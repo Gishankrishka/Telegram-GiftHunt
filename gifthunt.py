ea38c388626999bc1a912d3ba209185830bdb31c
Gishan Krishka <91087239+Gishankrishka@users.noreply.github.com>
2025-08-02 13:26:22 +0530

    Add files via upload

---
+++ gifthunt.py
@@ -0,0 +1,41 @@
+#Copyright  ©  t.me/gishandev
+
+
+from pyrogram import *
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+
+pbot = Client(
+    "gifthunt",
+    api_id=123456789,
+    api_hash="abcd1234",
+    phone_number="+94xxxxxxxx"
+)
+owner=
+max_price=500
+max_available_amount=15000
+
+async def check_star_gifts():
+    await pbot.send_message("krishdev", "working")
+    while True:
+        try:
+            gifts = await pbot.get_available_gifts()  # Fetch available gifts
+            for gift in gifts:
+                if not gift.is_sold_out and gift.price == max_price and gift.is_limited and gift.available_amount <= max_available_amount:
+                    await pbot.send_gift(
+                        chat_id=owner,
+                        gift=gift.id,
+                    )
+                    print(gift)
+                    break 
+        except Exception as e:
+            print(f"Error fetching or sending gifts: {e}")  
+
+if __name__ == "__main__":
+    try:
+        pbot.start()
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(check_star_gifts())
+        idle()
+    except Exception as e:
+        print(str(e))
